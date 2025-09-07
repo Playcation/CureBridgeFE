e96a586d4c7bf6becd3fd8c3101dc9e54a0f886f
genie <wlsdl7520@naver.com>
2025-09-07 17:33:29 +0900

    login, signup

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,7 +7,7 @@ export const store = configureStore({
   }
 });
 
-export default store;
+// export default store; 기본 내보내기 위의 store로 내보내기하고있어서, 중복 예방 주석처리
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
