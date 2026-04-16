aaea49ec6e7a22c19a9a5b2b1ced054ca9f7a344
‘JHabin’ <habin424@gmail.com>
2026-04-16 21:58:30 +0900

    fix. 답변여부 변수명 수정

--- src/types/support.ts
+++ src/types/support.ts
@@ -1,35 +1,35 @@
 export interface SupportRequest {
-    title: string;
-    content: string;
-    isPrivate: boolean;
+  title: string;
+  content: string;
+  isPrivate: boolean;
 }
 
 export interface SupportListItem {
-    supportId: number;
-    userId: number;
-    title: string;
-    content?: string;
-    isPrivate: boolean;
-    isRelied: boolean;
-    createdAt: string;
-    updatedAt?: string;
+  supportId: number;
+  userId: number;
+  title: string;
+  content?: string;
+  isPrivate: boolean;
+  isReplied: boolean;
+  createdAt: string;
+  updatedAt?: string;
 }
 
 export interface SupportDetail {
-    id: number;
-    title: string;
-    content: string;
-    isPrivate: boolean;
-    viewCount: number;
-    userId: number;
-    attachedFilePaths: string[];
+  id: number;
+  title: string;
+  content: string;
+  isPrivate: boolean;
+  viewCount: number;
+  userId: number;
+  attachedFilePaths: string[];
 
-    isReplied: boolean;
-    replyContent?: string | null;
-    repliedAt?: string | null;
+  isReplied: boolean;
+  replyContent?: string | null;
+  repliedAt?: string | null;
 }
 
 export interface ReplyRequest {
-    supportId: number;
-    replyContent: string;
+  supportId: number;
+  replyContent: string;
 }
